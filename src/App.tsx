1f11312955f88c99f3b8af438ee8e8b6a451b50b
muhammadBilalKhan-wq <theelonberg@gmail.com>
2026-02-07 15:16:20 +0500

    Redirect auth routes to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import { AppLayout } from './layouts/AppLayout';
-import { AuthPage } from './pages/AuthPage';
 import { CreatePage } from './pages/CreatePage';
 import { HomePage } from './pages/HomePage';
 import { NotificationsPage } from './pages/NotificationsPage';
@@ -19,8 +18,6 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/login" element={<AuthPage mode="login" />} />
-      <Route path="/register" element={<AuthPage mode="register" />} />
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
         <Route path="search" element={<SearchPage />} />
@@ -29,6 +26,11 @@ const App = () => {
         <Route path="profile/:username" element={<ProfilePage />} />
         <Route path="settings" element={<SettingsPage />} />
       </Route>
+      <Route path="/onboarding" element={<Navigate to="/" replace />} />
+      <Route path="/login" element={<Navigate to="/" replace />} />
+      <Route path="/signup" element={<Navigate to="/" replace />} />
+      <Route path="/register" element={<Navigate to="/" replace />} />
+      <Route path="/app" element={<Navigate to="/" replace />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
